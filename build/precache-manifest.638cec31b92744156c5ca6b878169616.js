80b621a6a24e3e04397023e6527588076d2ad83a
Yevhenii Deina <deinak1996@gmail.com>
2020-10-16 21:01:43 +0300

    a

---
+++ build/precache-manifest.638cec31b92744156c5ca6b878169616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd16cc5ef9cf7785f42c37de0a4e9a3f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "25489843ead76bb53b96",
+    "url": "/goit-react-hw-01-components/static/css/main.a72042fe.chunk.css"
+  },
+  {
+    "revision": "98b4e77914f91291a144",
+    "url": "/goit-react-hw-01-components/static/js/2.e8a16ae2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.e8a16ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25489843ead76bb53b96",
+    "url": "/goit-react-hw-01-components/static/js/main.5aa6882a.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
